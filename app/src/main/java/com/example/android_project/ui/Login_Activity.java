74645c9e309ca6d3e49c5a2dd987e35ce3f14214
arrya13n <vagh8940@mylaurier.ca>
2023-10-30 12:40:16 -0400

    validate email and password

--- app/src/main/java/com/example/android_project/ui/Login_Activity.java
+++ app/src/main/java/com/example/android_project/ui/Login_Activity.java
@@ -3,7 +3,9 @@ package com.example.android_project.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,28 +20,24 @@ public class Login_Activity extends AppCompatActivity {
     private EditText editTextPassword;
     private Button buttonLogin;
     private TextView textViewForgotPassword;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        SharedPreferences sharedpref = getSharedPreferences("SP", Login_Activity.MODE_PRIVATE);
+        editor = sharedpref.edit();
+        editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
+        String pass = sharedpref.getString("defaultpassword", "");
         buttonLogin = findViewById(R.id.buttonLogin);
         textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Implement your login logic here
-//                String email = editTextEmail.getText().toString();
-//                String password = editTextPassword.getText().toString();
-//                // Check email and password, and perform login
-
                 openMainActivity();
-
-
-
             }
         });
 
@@ -53,9 +51,26 @@ public class Login_Activity extends AppCompatActivity {
 
     }
      public void openMainActivity(){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-     }
+         String Text = editTextEmail.getText().toString();
+         String pass = editTextPassword.getText().toString();
+         if( !Patterns.EMAIL_ADDRESS.matcher(Text).matches())
+         {
+             Toast.makeText(this, "Enter a valid email", Toast.LENGTH_SHORT).show();
+         }
+         else if (pass.length() < 8) {
+             Toast.makeText(this, "Password length should be minimum 8", Toast.LENGTH_SHORT).show();
+         }
+         else if(!pass.matches(".*\\d.*") && !pass.matches(".*[!@#$%^&*()\\-+].*")){
+             Toast.makeText(this, "Password should have atleast one number and a special character", Toast.LENGTH_SHORT).show();
+         }
+         else if(pass.equals(pass.toLowerCase()) && pass.equals(pass.toUpperCase())){
+             Toast.makeText(this, "Password should have one upper and lower case letter", Toast.LENGTH_SHORT).show();
+         }
+         else {
+             Intent intent = new Intent(this, MainActivity.class);
+             startActivity(intent);
+         }
 
+     }
 
 }
